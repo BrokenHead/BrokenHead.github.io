37c33a445da84df74799d9e90fe1e394877a5858
BrokenHead <varist.siri@gmail.com>
2023-07-18 14:13:15 +0700

    test effect

--- app.js
+++ app.js
@@ -2,19 +2,19 @@
 const flightPath = {
     corviness: 1,
 
-    values:[
-        {x: -15, y: 20},
-        {x: -40, y: 500}
+    values: [
+        { x: -15, y: 20 },
+        { x: -40, y: 500 }
     ]
 }
 
 const tween = new TimelineLite();
 
 tween.add(
-    TweenLite.to(".choc_piece", 0.7 ,{
+    TweenLite.to(".choc_piece", 0.7, {
         bezier: flightPath,
         ease: Power1.easeIn,
-        rotation:270
+        rotation: 270
     })
 )
 
@@ -23,14 +23,14 @@ tween.add(
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
- triggerElement: '.animation',
- duration: 500,
- triggerHook:0
+    triggerElement: '.animation',
+    duration: 500,
+    triggerHook: 0
 })
-.setTween(tween)
-.addIndicators()
-.setPin(".animation")
-.addTo(controller)
+    .setTween(tween)
+    .addIndicators()
+    .setPin(".animation")
+    .addTo(controller)
 
 
 
@@ -45,21 +45,20 @@ const scene = new ScrollMagic.Scene({
 
 
 const clack_timeline = {
-    values:[
-        { opacity:1},
-        { opacity:0.5},
-        { opacity:0}
- 
+    values: [
+        { opacity: 1 },
+        { opacity: 0.5 },
+        { opacity: 0 }
+
     ]
 }
 
 const tween2 = new TimelineLite();
 
 tween2.add(
-    TweenLite.to(".choc_clack", 0.7 ,{
-        
+    TweenLite.to(".choc_clack", 0.7, {
         bezier: clack_timeline
-       
+
 
     })
 )
@@ -69,11 +68,11 @@ const controller2 = new ScrollMagic.Controller();
 var scene2 = new ScrollMagic.Scene({
     triggerElement: ".clack_start",
     duration: 200,
-    triggerHook:0
+    triggerHook: 0
 })
-.setTween(tween2) // trigger a TweenMax.to tween
-.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
-.addTo(controller2);
+    .setTween(tween2) // trigger a TweenMax.to tween
+    .addIndicators({ name: "1 (duration: 0)" }) // add indicators (requires plugin)
+    .addTo(controller2);
 
 
 
@@ -85,9 +84,9 @@ var scene2 = new ScrollMagic.Scene({
 
 // showing/hiding elements
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>{
+    entries.forEach((entry) => {
         console.log(entry)
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show")
         }
 
@@ -99,9 +98,9 @@ hiddenElements.forEach((el) => observer.observe(el))
 
 
 const observer_portf = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>{
+    entries.forEach((entry) => {
         console.log(entry)
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show_portf")
         }
 
