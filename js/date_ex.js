2d8cc074a0193ec41d06e9db8487fcac6a055345
BrokenHead <varist.siri@gmail.com>
2023-08-08 15:06:02 +0700

    update

--- js/date_ex.js
+++ js/date_ex.js
@@ -2,7 +2,7 @@ function date_ex(){
     let date_s = new Date("4/08/2022")   
     let date_now = new Date()
     var sub = (date_now.getTime() - date_s.getTime())/(24*60*60*1000) ;
-    document.getElementById("work_ex").innerHTML += Math.floor(sub/365) +" year " + Math.ceil((sub%365)/30) + " mos"
+    document.getElementById("work_ex").innerHTML += Math.floor(sub/365) +" year " + Math.ceil((sub%365)/30) + " month(s)"
 }
 // document.getElementById("work_ex").value = "test"
 date_ex()
